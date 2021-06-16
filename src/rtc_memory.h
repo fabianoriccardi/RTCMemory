a0f9b837b4b45cf2d581ba84f3ad30d59dc4a505
Fabiano Riccardi <fabiano.riccardi@outlook.com>
2021-06-16 21:55:13 +0200

    add license

--- src/rtc_memory.h
+++ src/rtc_memory.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright (C) 2020-2021  Fabiano Riccardi                             *
+ *                                                                         *
+ *   This file is part of RTC Memory.                                      *
+ *                                                                         *
+ *   RTC Memory is free software; you can redistribute                     *
+ *   it and/or modify it under the terms of the GNU Lesser General Public  *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   RTC Memory is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
 #ifndef RTC_MEMORY_H
 #define RTC_MEMORY_H
 
